ae235889aabba45e8752d0515f28980eb4cd353a
Carlos Díaz Girol <carlosdiazgirol@MacBook-Pro-de-Carlos.local>
2019-01-30 19:55:47 +0100

    fit: views register

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const User = require('../models/user.model');
 
 module.exports.register = (req, res, next) => {
-  res.render('users/register')
+  res.render('users/register', {layout: false});
 };
 module.exports.doCreate = (req, res, next) => {
 
 };
+
+
+
