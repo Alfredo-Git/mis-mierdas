0ddc33efe4045c7f3f14786cb76f1cc13ae4a912
Lucas <lucas@MacBook-Air-de-Lucas.local>
2019-02-13 19:00:50 +0100

    fit searchShits

--- public/pre/js/functions.js
+++ public/pre/js/functions.js
@@ -1,3 +1,5 @@
+console.log('a ver');
+
 $( document ).ready(function(){
   $(".fa-pencil-alt").click(function(){
     $(this).parents("li").find(".box-info").css("margin-top","-25px");
@@ -7,4 +9,22 @@ $( document ).ready(function(){
     $(this).parents("li").find(".box-info").css("margin-top","0");
     $(this).parents("li").find(".box-info-content").css("height","35px");
   })
+});
+
+$(document).ready(function(){
+  $('#searchBar').keyup(function(){
+     let cacas = $('.h2Names');
+     let buscandoCaca = $(this).val().toLowerCase();
+     let wc='';
+     for( var i = 0; i < cacas.length; i++ ){//<-----Hasta antes de aquí entra
+        wc = $(cacas[i]).html().toLowerCase();
+          for(var x = 0; x < wc.length; x++ ){
+              if( buscandoCaca.length == 0 || wc.indexOf( buscandoCaca ) > -1 ){
+                  $(cacas[i]).parents('li').show(); 
+              }else{
+                   $(cacas[i]).parents('li').hide();
+              }
+          }
+     }
+  });
 });
